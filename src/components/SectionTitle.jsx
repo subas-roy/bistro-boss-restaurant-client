19c69ebf6359f79cdca8de8379fc5fc292d2ac8a
subas-roy <subasroy46@gmail.com>
2025-08-13 08:40:37 +0600

    shared/reuseable section title for different sections done

---
+++ src/components/SectionTitle.jsx
@@ -0,0 +1,10 @@
+const SectionTitle = ({ subHeading, heading }) => {
+  return (
+    <div className="mx-auto text-center md:w-4/12 my-8">
+      <p className="text-yellow-600 mb-2">--- {subHeading} ---</p>
+      <h3 className="uppercase text-3xl border-y-2 py-4">{heading}</h3>
+    </div>
+  );
+};
+
+export default SectionTitle;
