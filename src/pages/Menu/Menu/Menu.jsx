0891b4823f742d91838b75c8b7c6675f02429b4c
subas-roy <subasroy46@gmail.com>
2025-08-16 14:37:49 +0600

    implement cover with blur parallax using react parallax

--- src/pages/Menu/Menu/Menu.jsx
+++ src/pages/Menu/Menu/Menu.jsx
@@ -1,8 +1,19 @@
+import Cover from "../../Shared/Cover/Cover";
+import menuImg from "../../../assets/menu/banner3.jpg";
+import PopularMenu from "../../Home/PopularMenu/PopularMenu.jsx"
+
 const Menu = () => {
   return (
     <div className="max-w-screen-xl mx-auto">
       <title>Bistro Boss | Menu</title>
-      <h2>Menu</h2>
+      <Cover img={menuImg} title={"Our Menu"}/>
+      <PopularMenu></PopularMenu>
+
+      <Cover img={menuImg} title={"Our Menu"}/>
+      <PopularMenu></PopularMenu>
+
+      <Cover img={menuImg} title={"Our Menu"}/>
+      <PopularMenu></PopularMenu>
     </div>
   );
 };
--- src/pages/Shared/Navbar/Navbar.jsx
+++ src/pages/Shared/Navbar/Navbar.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 const Navbar = () => {
   const navOptions = <>
     <li><Link to={'/'}>Home</Link></li>
-    <li><Link to={'/menu'}>Menu</Link></li>
+    <li><Link to={'/menu'}>Our Menu</Link></li>
     <li><a>Item 3</a></li>
   </>
   return (
