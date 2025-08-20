664693d32515b8b8e56f3e4dc174766807d54656
subas-roy <subasroy46@gmail.com>
2025-08-20 09:53:19 +0600

    hide Navbar and Footer on login page

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -1,13 +1,16 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import Footer from "../pages/Shared/Footer/Footer";
 import Navbar from "../pages/Shared/Navbar/Navbar";
 
 const Main = () => {
+  // Hide Navbar and Footer on login page
+  const location = useLocation();
+  const noHeaderFooter = location.pathname.includes('login');
   return (
     <div>
-      <Navbar />
+      {noHeaderFooter || <Navbar />}
       <Outlet />
-      <Footer />
+      {noHeaderFooter || <Footer />}
     </div>
   );
 };
---
+++ src/pages/Login/Login.jsx
@@ -0,0 +1,10 @@
+
+const Login = () => {
+  return (
+    <div className="max-w-screen-xl mx-auto">
+      <h2>Login Page</h2>
+    </div>
+  );
+};
+
+export default Login;
