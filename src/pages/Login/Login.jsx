1011f511ce9f818d3d46f33ed627d47ef38f5b0e
subas-roy <subasroy46@gmail.com>
2025-08-20 16:41:15 +0600

    setup recaptcha and validate captcha

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,13 +1,34 @@
+import { useEffect, useRef, useState } from 'react';
+import { loadCaptchaEnginge, LoadCanvasTemplate, validateCaptcha } from 'react-simple-captcha';
 
 const Login = () => {
+  const [disabled, setDisabled] = useState(true);
+
+  const captchaRef = useRef(null);
+
+  useEffect(() => {
+    loadCaptchaEnginge(6)
+  })
+
   const handleLogin = event => {
     event.preventDefault();
     const form = event.target;
     const email = form.email.value;
     const password = form.password.value;
     console.log({ email, password })
-
   }
+
+  const handleValidateCaptcha = (e) => {
+    e.preventDefault();
+    const user_captcha_value = captchaRef.current.value;
+    if (validateCaptcha(user_captcha_value)) {
+      setDisabled(false)
+    }
+    else {
+      setDisabled(true)
+    }
+  }
+
   return (
     <div className="max-w-screen-xl mx-auto hero bg-base-200 min-h-screen">
       <div className="hero-content flex-col lg:flex-row-reverse">
@@ -26,7 +47,10 @@ const Login = () => {
               <label className="label">Password</label>
               <input type="password" name="password" className="input" placeholder="Password" />
               <div><a className="link link-hover">Forgot password?</a></div>
-              <button className="btn btn-neutral mt-4">Login</button>
+              < LoadCanvasTemplate />
+              <input type="text" ref={captchaRef} name="captcha" className="input" placeholder="type the captcha above" />
+              <button onClick={handleValidateCaptcha} className="btn btn-neutral btn-xs btn-outline">Validate</button>
+              <button disabled={disabled} className="btn btn-neutral mt-4">Login</button>
               <input type="submit" value="Login" />
             </fieldset>
           </form>
