9071a060edd2c8e5de0d947fecb23728f5c585f7
subas-roy <subasroy46@gmail.com>
2025-08-20 13:33:04 +0600

    simple login form

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,8 +1,37 @@
 
 const Login = () => {
+  const handleLogin = event => {
+    event.preventDefault();
+    const form = event.target;
+    const email = form.email.value;
+    const password = form.password.value;
+    console.log({ email, password })
+
+  }
   return (
-    <div className="max-w-screen-xl mx-auto">
-      <h2>Login Page</h2>
+    <div className="max-w-screen-xl mx-auto hero bg-base-200 min-h-screen">
+      <div className="hero-content flex-col lg:flex-row-reverse">
+        <div className="text-center md:w-1/2 lg:text-left">
+          <h1 className="text-5xl font-bold">Login now!</h1>
+          <p className="py-6">
+            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
+            quasi. In deleniti eaque aut repudiandae et a id nisi.
+          </p>
+        </div>
+        <div className="card lg:w1/2 bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+          <form onSubmit={handleLogin} className="card-body">
+            <fieldset className="fieldset">
+              <label className="label">Email</label>
+              <input type="email" name="email" className="input" placeholder="Email" />
+              <label className="label">Password</label>
+              <input type="password" name="password" className="input" placeholder="Password" />
+              <div><a className="link link-hover">Forgot password?</a></div>
+              <button className="btn btn-neutral mt-4">Login</button>
+              <input type="submit" value="Login" />
+            </fieldset>
+          </form>
+        </div>
+      </div>
     </div>
   );
 };
