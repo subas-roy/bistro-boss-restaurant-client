25327dccde6a00cfcaae6b4cc278f512ff209a69
subas-roy <subasroy46@gmail.com>
2025-08-22 09:35:32 +0600

    react hook form validation, error handling

--- src/pages/SignUp/SignUp.jsx
+++ src/pages/SignUp/SignUp.jsx
@@ -1,5 +1,14 @@
+import { useForm } from "react-hook-form";
 
 const SignUp = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = (data) => console.log(data)
+
   return (
     <div className="max-w-screen-xl mx-auto hero min-h-screen">
       <div className="hero-content flex-col lg:flex-row-reverse">
@@ -11,17 +20,30 @@ const SignUp = () => {
           </p>
         </div>
         <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
-          <div className="card-body">
+          <form onSubmit={handleSubmit(onSubmit)} className="card-body">
             <fieldset className="fieldset">
+
               <label className="label">Name</label>
-              <input type="text" name="name" className="input" placeholder="Name" />
+              <input type="text" {...register("name", { required: true })} name="name" className="input" placeholder="Name" />
+              {errors.name && <span className="text-red-600">Name is required</span>}
+
               <label className="label">Email</label>
-              <input type="email" name="email" className="input" placeholder="Email" />
+              <input type="email" {...register("email", { required: true })} name="email" className="input" placeholder="Email" />
+              {errors.email && <span className="text-red-600">Email is required</span>}
+
               <label className="label">Password</label>
-              <input type="password" name="password" className="input" placeholder="Password" />
+              <input type="password" {...register("password", {
+                required: "Password is required",
+                minLength: {
+                  value: 6,
+                  message: "Password must be at least 6 characters long.",
+                },
+              })} name="password" className="input" placeholder="Password" />
+              {errors.password && <p className="text-red-600">{errors.password.message}</p>}
+
               <button className="btn btn-neutral mt-4">Sign up</button>
             </fieldset>
-          </div>
+          </form>
         </div>
       </div>
     </div>
