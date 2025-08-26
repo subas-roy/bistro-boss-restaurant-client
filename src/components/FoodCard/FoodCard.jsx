93c5bdecb3c61772e20de04189df773525a41f2f
subas-roy <subasroy46@gmail.com>
2025-08-26 16:05:28 +0600

    add axiosSecure instance with custom hook

--- src/components/FoodCard/FoodCard.jsx
+++ src/components/FoodCard/FoodCard.jsx
@@ -1,7 +1,7 @@
 import Swal from "sweetalert2";
 import useAuth from "../../hooks/useAuth";
 import { useLocation, useNavigate } from "react-router-dom";
-import axios from "axios";
+import { axiosSecure } from "../../hooks/useAxiosSecure";
 
 const FoodCard = ({ item }) => {
   const { name, image, price, recipe, _id } = item;
@@ -19,7 +19,7 @@ const FoodCard = ({ item }) => {
         image,
         price
       }
-      axios.post('http://localhost:5000/carts', cartItem)
+      axiosSecure.post('/carts', cartItem)
         .then(res => {
           console.log(res.data);
           if (res.data.insertedId) {
